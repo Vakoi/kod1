2ecb64973b8d394123286ce3a1c8ad66fa5b1ea0
Vakoi <mak-uk@mail.ru>
2023-04-17 15:58:56 +0300

    Добавьте файлы проекта.

---
+++ Практика день 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 20; // замените на нужное значение
+        double b = 0; // замените на нужное значение
+
+        double averageArithmetic = (a + b) / 2;
+        double averageGeometric = Math.Sqrt(a * b);
+
+        Console.WriteLine("Среднее арифметическое: " + averageArithmetic);
+        Console.WriteLine("Среднее геометрическое: " + averageGeometric);
+    }
+}
